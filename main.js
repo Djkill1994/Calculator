2b7692df3419a5e79aa9428ca8212562092bed36
Bruyok Vlad <djkill911@gmail.com>
2021-09-25 14:00:32 +0300

    Redesigned layout, added functions for reducing text when adding, rewritten function for adding information output, only numbers are displayed.

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const dataInput = document.querySelector('.data-input');
 const gridTable = document.querySelector('.grid');
+const assignmentOperator = document.querySelector('.assignment-operator');
+const arithmeticOperator = document.querySelector('.arithmetic-operator');
+const gridNumber = document.querySelector('.number');
+let fontSize = 90;
 
-function myFunc(event){
-    const valueTarget = (event.target.attributes.value.value)
-    return valueTarget
+function dataInputValue(event){
+    let valueData = event.target.attributes.value.value;
+    if(valueData === String(Number(valueData))) {
+        dataInput.innerHTML += valueData;
+    }else if (valueData === '.'){
+        dataInput.innerHTML += valueData;
+    }
 }
 
-// function addToNumber (){
-//     myFunc(valueTarget)
-//     console.log(valueTarget)
-// }
+function fontSizeToNumber (){
+    dataInputValue(event);
+    if (dataInput.innerHTML.length > 9) {
+        if (fontSize > 62) {
+            fontSize = fontSize - 7
+        }else if (fontSize > 44){
+            fontSize = fontSize-3
+        }else if(fontSize > 35){
+            fontSize = fontSize-2
+        }else if(fontSize > 17){
+            fontSize = fontSize-1
+        }else if (dataInput.innerHTML.length >50) {
 
-gridTable.addEventListener('click', myFunc, addToNumber)
+        }
+        dataInput.style.fontSize = ('font-size', fontSize + 'px');
+    }
+}
+
+gridTable.addEventListener('click', fontSizeToNumber)
 
 
