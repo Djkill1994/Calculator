6b3a70a78a3539768e3bc562e603a964482aed59
Bruyok Vlad <djkill911@gmail.com>
2021-10-26 22:04:38 +0300

    Completion of the code, elimination of errors was made

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const dataInput = document.querySelector('.data-input');
 const gridTable = document.querySelector('.grid');
-const plusOperator = document.getElementById('plus');
-const minusOperator = document.getElementById('minus');
-const divisionOperator = document.getElementById('division');
-const multiplyOperator = document.getElementById('multiplication');
-const equalsOperator = document.getElementById('equals');
-const acOperator = document.getElementById('ac');
+const plusOperator = document.querySelector('#plus');
+const minusOperator = document.querySelector('#minus');
+const divisionOperator = document.querySelector('#division');
+const multiplyOperator = document.querySelector('#multiplication');
+const equalsOperator = document.querySelector('#equals');
+const acOperator = document.querySelector('#ac');
+const addingMinus = document.querySelector('#add-minus');
+const interestOperator = document.querySelector('#interest');
+let pointEntry = document.querySelector('#point');
 
 let fontSize = 90;
 let fistNumber;
 let secondNumber;
 let result;
 
+const NUMBER_OPERATORS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-const NUMBER_OPERATORS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-
-function ac() {
-    result = 0;
-    fistNumber = 0;
-    secondNumber = 0;
-    dataInput.innerHTML = '';
+function operatorsRefresh (){
     plusOperator.style.backgroundColor = 'rgba(254,159,11,1)';
     plusOperator.style.color = 'white';
     minusOperator.style.backgroundColor = 'rgba(254,159,11,1)';
@@ -28,6 +26,27 @@ function ac() {
     divisionOperator.style.color = 'white';
     multiplyOperator.style.backgroundColor = 'rgba(254,159,11,1)';
     multiplyOperator.style.color = 'white';
+}
+
+function ac() {
+    result = 0;
+    fistNumber = 0;
+    secondNumber = 0;
+    dataInput.innerHTML = '0';
+    dataInput.style.fontSize = 90 + 'px';
+    operatorsRefresh()
+}
+
+function interest(){
+    let interestResult = +fistNumber / 100 * +secondNumber;
+    result = +fistNumber - interestResult;
+    dataInput.innerHTML = result;
+}
+
+function addingMinusToDataInput(){
+
+
+    dataInput.innerHTML = '-' + dataInput.innerHTML;
 }
 
 function plus() {
@@ -47,6 +66,7 @@ function plus() {
         plusOperator.style.color = 'rgba(254,159,11,1)';
     }
     dataInput.innerHTML = ''
+    dataInput.style.fontSize = 90 + 'px';
 }
 
 function minus() {
@@ -66,6 +86,7 @@ function minus() {
         minusOperator.style.color = 'rgba(254,159,11,1)';
     }
     dataInput.innerHTML = ''
+    dataInput.style.fontSize = 90 + 'px';
 }
 
 function division (){
@@ -85,6 +106,7 @@ function division (){
         divisionOperator.style.color = 'rgba(254,159,11,1)';
     }
     dataInput.innerHTML = ''
+    dataInput.style.fontSize = 90 + 'px';
 }
 
 function multiply (){
@@ -104,6 +126,7 @@ function multiply (){
         multiplyOperator.style.color = 'rgba(254,159,11,1)';
     }
     dataInput.innerHTML = ''
+    dataInput.style.fontSize = 90 + 'px';
 }
 
 function equals() {
@@ -120,15 +143,30 @@ function equals() {
         result = +fistNumber * +secondNumber;
         dataInput.innerHTML = result;
     }
+    fontSizeToNumber()
+    operatorsRefresh()
+}
 
+function addingPoint (){
+    if (dataInput.innerHTML === '0'|| dataInput.innerHTML === ''){
+        dataInput.innerHTML = '0.'
+    }else if (dataInput.innerHTML.endsWith('.') === true){
+        dataInput.innerHTML
+    }else if (NUMBER_OPERATORS) {
+        dataInput.innerHTML += '.'
+    }
 }
 
 function arithmeticOperation(event) {
     const inputValue = event.target.innerHTML;
     if (NUMBER_OPERATORS.includes(inputValue)) {
+        if (dataInput.innerHTML === '0'){
+            dataInput.innerHTML = ''
+        }
         dataInput.innerHTML += inputValue;
         secondNumber = dataInput.innerHTML
         fontSizeToNumber();
+
         console.log(secondNumber)
         console.log(fistNumber)
     }
@@ -137,7 +175,7 @@ function arithmeticOperation(event) {
 function fontSizeToNumber() {
     if (dataInput.innerHTML.length > 8) {
         if (fontSize > 62) {
-            fontSize = fontSize - 7;
+            fontSize = fontSize - 8;
         } else if (fontSize > 48) {
             fontSize = fontSize - 3;
         } else if (fontSize > 40) {
@@ -147,6 +185,16 @@ function fontSizeToNumber() {
         }
         dataInput.style.fontSize = fontSize + 'px';
     }
+    // if (dataInput.innerHTML.length > 2){
+    //     let warningDiv = document.createElement('div')
+    //     warningDiv.style.width = '100px';
+    //     warningDiv.style.height = '100px';
+    //     warningDiv.style.backgroundColor = 'gray';
+    //     warningDiv.innerHTML = 'It is impossible to enter more than 15 numbers';
+    //     document.body.appendChild(warningDiv);
+    //     alert('div')
+    // }
+    console.log(fontSize);
 }
 
 gridTable.addEventListener('click', arithmeticOperation);
@@ -155,4 +203,7 @@ equalsOperator.addEventListener('click', equals);
 acOperator.addEventListener('click', ac);
 minusOperator.addEventListener('click', minus);
 divisionOperator.addEventListener('click', division);
-multiplyOperator.addEventListener('click', multiply)
+multiplyOperator.addEventListener('click', multiply);
+pointEntry.addEventListener('click' , addingPoint);
+interestOperator.addEventListener('click', interest);
+addingMinus.addEventListener('click', addingMinusToDataInput)
